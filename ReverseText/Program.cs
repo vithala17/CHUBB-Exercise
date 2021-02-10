f3e6c746790c1ba75410c5d5a6c454b443e2d400
vithala17 <40426095+vithala17@users.noreply.github.com>
2021-02-10 23:11:16 +0530

    added code

---
+++ ReverseText/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ReverseText
+{
+    // IMPORTANT: make sure you read the instructions in README.md
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filePath = "TestTextFile.txt";
+            
+            // string text = ... call FileTextReverser to reverse the files text
+
+            // Console.WriteLine(text);
+        }
+    }
+
+    public class FileTextReverser
+    {
+        public string ReverseFileContents(string filePath)
+        {
+            // TODO: read text file, reverse the text, save reversed text back to the same file
+
+            // TODO: return the reversed text
+            return null;
+        }
+    }
+}
