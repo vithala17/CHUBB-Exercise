5d47123e14de51a393773a888ba5115dabde79ab
vithala17 <40426095+vithala17@users.noreply.github.com>
2021-02-11 13:11:07 +0530

    Update Program.cs

--- ReverseText/Program.cs
+++ ReverseText/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace ReverseText
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ReverseText
 {
     // IMPORTANT: make sure you read the instructions in README.md
 
@@ -7,10 +12,11 @@
         static void Main(string[] args)
         {
             const string filePath = "TestTextFile.txt";
-            
-            // string text = ... call FileTextReverser to reverse the files text
+            FileTextReverser textReverser = new FileTextReverser();
 
-            // Console.WriteLine(text);
+            string text = textReverser.ReverseFileContents(filePath);
+
+            Console.WriteLine(text);
         }
     }
 
@@ -18,10 +24,47 @@
     {
         public string ReverseFileContents(string filePath)
         {
-            // TODO: read text file, reverse the text, save reversed text back to the same file
+            try
+            {
+                string revText, newString = "";
+                // TODO: read text file, reverse the text, save reversed text back to the same file
 
-            // TODO: return the reversed text
-            return null;
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    revText = reader.ReadToEnd();
+                    var revTextArray = revText.ToCharArray();
+                    Array.Reverse(revTextArray);
+
+                    newString = String.Concat(revTextArray);
+                }
+
+                filePath = "OutputTextFile.txt";
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    //writer.Write(newString);
+                    //writer.Flush();
+
+                    writer.WriteLine("C# Corner Authors");
+                    writer.WriteLine("==================");
+                    writer.WriteLine("Monica Rathbun");
+                    writer.WriteLine("Vidya Agarwal");
+                    writer.WriteLine("Mahesh Chand");
+                    writer.WriteLine("Vijay Anand");
+                    writer.WriteLine("Jignesh Trivedi");
+
+                    writer.Flush();
+                    writer.Close();
+                }
+
+                // TODO: return the reversed text
+                return newString;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
         }
     }
 }
